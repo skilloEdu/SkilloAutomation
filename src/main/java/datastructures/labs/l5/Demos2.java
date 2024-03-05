79d216170551da029dad43f6bb3bcd9d091574be
Nikolay Nikolov <skilloeducation@abv.bg>
2024-03-05 13:25:49 +0200

    Adding some more code extra 1

---
+++ src/main/java/datastructures/labs/l5/Demos2.java
@@ -0,0 +1,10 @@
+package main.java.datastructures.labs.l5;
+
+public class Demos2 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Adding some more code here");
+
+    }
+}
